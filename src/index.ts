f3a0b50b89315050fe3dffe28558e52988d1eb1f
James Aguirre <jamesaguirre98@gmail.com>
2023-08-30 00:17:59 -0700

    finished db user schema

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -22,3 +23,10 @@ const server = http.createServer(app)
 server.listen(8080, () => {
     console.log('Server listening on port 8080')
 });
+
+const   MONGO_URL = 'mongodb+srv://jamesaguirre:7SPK7JvEF0gkHM2H@cluster0.wv1cydi.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error))
+
